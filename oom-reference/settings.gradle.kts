1717a41b94ee93807002804e9bad8112fd6f8a4d
PARK SUNG DONG <oraedoa@gmail.com>
2023-01-20 18:29:09 +0900

    study

---
+++ oom-reference/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "oom-reference"
---
+++ oom-reference/src/test/kotlin/com/example/oomreference/OomReferenceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oomreference
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OomReferenceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
